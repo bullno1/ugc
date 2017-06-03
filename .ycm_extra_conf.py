43dd4dbb2d4271d4b1acab36e82e78b265cea90b
Bach Le <bach@bullno1.com>
2017-06-03 17:19:13 +0800

    Move munit to deps

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -44,7 +44,7 @@ flags = [
 '-fexceptions',
 '-DNDEBUG',
 '-DUGC_IMPLEMENTATION',
-'-I', '.',
+'-I', 'deps',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
